fb0cd0fda04a7f95c7e6350db1d1a83fd84f038d
Tatyana Sender <tatsiana.sender@cogniteq.com>
2025-02-09 19:46:33 +0100

    feat: add router and NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react';
 import Home from './pages/Home/Home';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './pages/NotFound/NotFound';
 
 const App: FC = () => {
   return (
-    <ErrorBoundary>
-      <Home />
-    </ErrorBoundary>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ErrorBoundary>
+    </BrowserRouter>
   );
 };
 
