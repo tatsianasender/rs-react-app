988edbb9c2bf6291d456c899a792f43e0297acd9
Tatyana Sender <tatsiana.sender@cogniteq.com>
2025-02-23 13:14:13 +0100

    feat: add context to control the application theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { useTheme } from './ThemeContext';
 import Home from './pages/Home/Home';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -6,15 +7,22 @@ import NotFound from './pages/NotFound/NotFound';
 import Details from './components/Details';
 
 const App: FC = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <BrowserRouter>
       <ErrorBoundary>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="details/:id" element={<Details />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div className={theme}>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="details/:id" element={<Details />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <button className="btn_theme" onClick={toggleTheme}>
+            Theme {theme}
+          </button>
+        </div>
       </ErrorBoundary>
     </BrowserRouter>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { ThemeProvider } from './ThemeContext.tsx';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </StrictMode>
   );
 }
